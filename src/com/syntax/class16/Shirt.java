8fb9fce8c3329ba35b094bda0f39c3fe6a137f08
IgorKostiukov <igorek2309@gmail.com>
2022-06-14 17:03:45 -0500

    constructor

---
+++ src/com/syntax/class16/Shirt.java
@@ -0,0 +1,13 @@
+package com.syntax.class16;
+
+public class Shirt {
+    public static String color;
+    public static char size;
+    Shirt() {}
+    Shirt(String newColor, char newSize){
+        color=newColor;
+        size=newSize;
+
+    }
+
+}
