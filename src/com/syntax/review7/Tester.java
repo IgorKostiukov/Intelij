7dd4280218902d20fcc486e77d6573e8ea07e7ad
IgorKostiukov <igorek2309@gmail.com>
2022-06-16 20:57:29 -0500

    reviewlesson

---
+++ src/com/syntax/review7/Tester.java
@@ -0,0 +1,9 @@
+package com.syntax.review7;
+
+public class Tester {
+    public static void main(String[] args) {
+        B13String m1=new B13String("Hello Bro");
+        int count=m1.countChars('l');
+        System.out.println(count);
+    }
+}
